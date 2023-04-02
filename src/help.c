3712b132638d9bab592a8e5a08dd3917461eff46
Paul FT <pfrelandthiery@gmail.com>
2023-04-02 21:34:21 +0100

    better logging

--- src/help.c
+++ src/help.c
@@ -2,5 +2,5 @@
 
 void print_help()
 {
-	printf("Usage : tavm -m <memory in mib> [-l] <log level>\n[-l] - set log level : trace (0), debug (1), info (2), warn (3), erorr (4), fatal (5), disable\nFor more informations, please refer to documentations (wip).\n");
+	printf("Usage : tavm -m <memory in bytes> (-l) <log level> | [-h] \n [-h] -> help \n [-l] ->  set log level : trace (0), debug (1), info(2 - default), warn (3), erorr (4), fatal (5), disable (6)\nFor more informations, please refer to documentations (wip).\n");
 }
